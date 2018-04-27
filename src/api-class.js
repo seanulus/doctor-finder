da72a4ed84125a2535065ae566a75153f2227344
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 09:46:55 -0700

    Add API test functioning

--- src/api-class.js
+++ src/api-class.js
@@ -2,7 +2,7 @@ class API {
   makeCall() {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2018-04-27/doctors?location=Portland,or,us&limit=10&user_key=${exports.apikey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.apiKey}`
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,4 +17,4 @@ class API {
   }
 }
 
-export { API  };
+export { API };
