51823ce63dd1c8d0766892f29fd1aefc61e4561d
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 15:31:24 -0700

    Add error message for failed call

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 
-
+// let errorMessage = function() {
+//     $("#doctorInfo").append("Sorry, there were no results for your query. Please try again")
+// }
 
 $(document).ready(function() {
   $('#doctorData').submit(function(event) {
@@ -16,10 +18,20 @@ $(document).ready(function() {
     api.nameCall(name, symptom).then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
-
+      debugger;
       for (var i = 0; i < body.data.length; i++) {
-        $("#doctorInfo").append(`<li>First Name: ${body.data[i].profile.first_name}<br>Last name: ${body.data[i].profile.last_name}<br>Address: ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}<br>${body.data[i].practices[0].visit_address.street}<br>${body.data[i].practices[0].visit_address.zip}<br>Phone Number: ${body.data[i].practices[0].phones[0].number}<br>Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}<br>Website: <a href="${body.data[i].practices[0].website}">Click Here</a></li>`);
-      }
+        $("#doctorInfo").append(`<li>First Name: ${body.data[i].profile.first_name}<br>
+                                     Last name: ${body.data[i].profile.last_name}<br>
+                                     Address: ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}<br>
+                                     ${body.data[i].practices[0].visit_address.street}<br>
+                                     ${body.data[i].practices[0].visit_address.zip}<br>
+                                     Phone Number: ${body.data[i].practices[0].phones[0].number}<br>
+                                     Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}<br>
+                                     Website:
+                                     <a href="${body.data[i].practices[0].website}">Click Here</a></li>`);
+                                   }
+    },  function(error) {
+        $("#showError").text(`There was an error processing your request: ${error.message}`)
     });
   });
 });
