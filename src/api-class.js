14dca759fc6b2a2c45fe1aa6b171bd34608515d5
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 16:34:08 -0700

    Add minor fixes

--- src/api-class.js
+++ src/api-class.js
@@ -2,7 +2,7 @@ class API {
   nameCall(name, symptom) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=b9f379f487f43ab661ce9e68592def84`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.apiKey}`
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
