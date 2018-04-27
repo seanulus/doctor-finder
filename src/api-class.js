c4079fdb8ea11763bbe0be13df97fb8ceb74497e
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 09:17:30 -0700

    Add API call not functioning

--- src/api-class.js
+++ src/api-class.js
@@ -2,7 +2,7 @@ class API {
   makeCall() {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/`
+      let url = `https://api.betterdoctor.com/2018-04-27/doctors?location=Portland,or,us&limit=10&user_key=${exports.apikey}`
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
