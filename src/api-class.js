810015027faf0171f47525a1afd5dc3678129d11
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 12:00:42 -0700

    Add functioning API call for names

--- src/api-class.js
+++ src/api-class.js
@@ -1,8 +1,8 @@
 class API {
-  makeCall() {
+  nameCall(name) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.apiKey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=2&limit=10&user_key=b9f379f487f43ab661ce9e68592def84`
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
