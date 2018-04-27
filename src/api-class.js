3b313815934159ab1b32835f5e29ee9d4de1d801
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 13:40:56 -0700

    Add query to API call for symptom check

--- src/api-class.js
+++ src/api-class.js
@@ -1,8 +1,8 @@
 class API {
-  nameCall(name) {
+  nameCall(name, symptom) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=2&limit=10&user_key=b9f379f487f43ab661ce9e68592def84`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=b9f379f487f43ab661ce9e68592def84`
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
