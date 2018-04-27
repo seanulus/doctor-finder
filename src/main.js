1ad91951a3627828238cb0fd086b67f88d598fb9
Sean McDermott <seanjmac@hotmail.com>
2018-04-27 13:49:21 -0700

    Add symptom variable for API call

--- src/main.js
+++ src/main.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
     $('#doctorInfo').empty();
     let api = new API();
     let name = $("#doctorName").val().toUpperCase();
-
-    api.nameCall(name).then(function(response) {
+    let symptom = $("#symptomName").val().toUpperCase();
+    api.nameCall(name, symptom).then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
 
       for (var i = 0; i < body.data.length; i++) {
-        $("#doctorInfo").append(`<li>First Name: ${body.data[i].profile.first_name}<br>Last name: ${body.data[i].profile.last_name}<br>Address: ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}<br>${body.data[i].practices[0].visit_address.street}<br>${body.data[i].practices[0].visit_address.zip}<br>Phone Number: ${body.data[i].practices[0].phones[0].number}<br>Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`)
+        $("#doctorInfo").append(`<li>First Name: ${body.data[i].profile.first_name}<br>Last name: ${body.data[i].profile.last_name}<br>Address: ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}<br>${body.data[i].practices[0].visit_address.street}<br>${body.data[i].practices[0].visit_address.zip}<br>Phone Number: ${body.data[i].practices[0].phones[0].number}<br>Accepting Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`);
       }
     });
   });
